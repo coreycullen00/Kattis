711b5e906d2ef32f3aee81467f64d209c45abd5f
Corey Cullen <coreycullen00@live.ie>
2024-09-15 16:48:43 +0100

    SodaSlurper

---
+++ problems/SodaSlurper.kt
@@ -0,0 +1,18 @@
+package problems
+
+fun main() {
+    val (e, f, c) = readln().split(" ").map { it.toInt() }
+    val empty = e + f
+    val drank = drinkAndBuyBottles(empty, c)
+    println(drank)
+}
+
+private fun drinkAndBuyBottles(empty: Int, cost: Int): Int {
+    var e = empty
+    if (e < cost) {
+        return 0
+    }
+    e = empty - cost + 1
+
+    return 1 + drinkAndBuyBottles(e, cost)
+}
